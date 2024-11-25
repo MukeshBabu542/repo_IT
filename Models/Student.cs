509f69344d5420c5a6113349269a89f298d5845d
Mukesh Babu Acharya <universalbabu77@gmail.com>
2024-11-25 08:23:17 +0545

    first commit

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int STUDENTID { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime Enrollmentdate { get; set; }
+        public string Address { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
